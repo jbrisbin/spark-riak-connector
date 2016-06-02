a01bb1b1fae02cc63d2a4fec47f735ed156a7a4f
Sean Jensen-Grey <sjensengrey@basho.com>
2016-06-02 16:14:23 -0500

    patch class or instance

--- connector/python/pyspark_riak/pyspark_patch.py
+++ connector/python/pyspark_riak/pyspark_patch.py
@@ -2,10 +2,10 @@ import pyspark
 import types
 from .riak_rdd import saveToRiak, riakBucket
 
-def riak_context():
-    pyspark.context.SparkContext.riakBucket = riakBucket
-    pyspark.rdd.RDD.saveToRiak = saveToRiak
-
-def riak_context(context):
-    context.riakBucket = types.MethodType(riakBucket, context)
-    pyspark.rdd.RDD.saveToRiak = saveToRiak
+def riak_context(context=None):
+    if context==None:
+        pyspark.context.SparkContext.riakBucket = riakBucket
+        pyspark.rdd.RDD.saveToRiak = saveToRiak
+    else:
+        context.riakBucket = types.MethodType(riakBucket, context)
+        pyspark.rdd.RDD.saveToRiak = saveToRiak
