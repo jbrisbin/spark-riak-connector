95feb6fde7065661dc91a2d4fff4be5e57fa1899
Jon Brisbin <jon@jbrisbin.com>
2016-08-15 13:30:14 -0500

    Tweak publish step

--- travis/build.sh
+++ travis/build.sh
@@ -3,11 +3,12 @@
 SBT_CMD="sbt $PBC_OPTS ++$TRAVIS_SCALA_VERSION"
 
 if [ "$RIAK_FLAVOR" == "riak-kv" ]; then
-  $SBT_CMD runRiakKVTests;
+  $SBT_CMD runRiakKVTests
 else
-  $SBT_CMD test;
+  $SBT_CMD test
 fi
 
-if [[ "develop" == "$TRAVIS_BRANCH" ] || [ "master" == "$TRAVIS_BRANCH" ]] && [ "false" == "$TRAVIS_PULL_REQUEST" ]; then
-  $SBT_CMD sparkRiakConnector/publishSigned;
+if [ "develop" == "$TRAVIS_BRANCH" -o "master" == "$TRAVIS_BRANCH" ] && [ "false" == "$TRAVIS_PULL_REQUEST" ]; then
+  openssl aes-256-cbc -K $encrypted_e4898ca93742_key -iv $encrypted_e4898ca93742_iv -in secrets.tgz.enc -out secrets.tgz -d && tar -zxf secrets.tgz
+  $SBT_CMD sparkRiakConnector/publishSigned
 fi
